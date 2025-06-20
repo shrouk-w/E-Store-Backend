8aa4bf8e95012a6ade8da08fffeb09dd92a0c036
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-06-20 18:07:29 +0200

    added JWT service

---
+++ store/src/main/java/com/springcourse/store/api/services/JWTService.java
@@ -0,0 +1,7 @@
+package com.springcourse.store.api.services;
+
+import com.springcourse.store.api.model.LocalUser;
+
+public interface JWTService {
+    public String generateJWT(LocalUser user);
+}
