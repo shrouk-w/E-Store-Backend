82555615a2b77a02d098628700c58d20cbe33389
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-06-20 18:21:20 +0200

    added additional validation

--- store/src/main/java/com/springcourse/store/api/model/dtos/LoginBody.java
+++ store/src/main/java/com/springcourse/store/api/model/dtos/LoginBody.java
@@ -1,8 +1,12 @@
 package com.springcourse.store.api.model.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginBody {
 
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 
     public String getUsername() {
