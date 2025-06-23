6b9a780e6238132b15c3dea9b9330225b4287683
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-06-23 12:40:06 +0200

    added proper auth to endpoints

--- store/src/main/java/com/springcourse/store/api/security/WebSecurityConfig.java
+++ store/src/main/java/com/springcourse/store/api/security/WebSecurityConfig.java
@@ -5,17 +5,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 import org.springframework.stereotype.Component;
 
 @Configuration
 public class WebSecurityConfig {
 
+    JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors->cors.disable())
-            .authorizeHttpRequests(authorizeRequests ->authorizeRequests.anyRequest().permitAll());
+            .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
+            .cors(cors -> cors.disable())
+            .authorizeHttpRequests(auth ->
+                {auth
+                        .requestMatchers("/product").permitAll()
+                        .requestMatchers("/auth/register").permitAll()
+                        .requestMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated();
+                });
         return http.build();
     }
 
