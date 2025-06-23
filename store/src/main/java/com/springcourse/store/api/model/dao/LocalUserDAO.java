a9b13efc1a722158f14ed160619bd9fd716ab56d
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-06-23 11:52:43 +0200

    added quantity field and changed dao to list crud

--- store/src/main/java/com/springcourse/store/api/model/dao/LocalUserDAO.java
+++ store/src/main/java/com/springcourse/store/api/model/dao/LocalUserDAO.java
@@ -2,10 +2,11 @@ package com.springcourse.store.api.model.dao;
 
 import com.springcourse.store.api.model.LocalUser;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 
 import java.util.Optional;
 
-public interface LocalUserDAO extends CrudRepository<LocalUser, Long> {
+public interface LocalUserDAO extends ListCrudRepository<LocalUser, Long> {
 
 
     Optional<LocalUser> findByUsernameIgnoreCase(String username);
